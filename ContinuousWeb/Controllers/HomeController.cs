1745e276e6a5ecf40b26fde0fef5f8da920e4ae1
Alexander Thomas Flick <ATFlick@COEA.local>
2018-01-09 16:35:10 +0100

    app insights functionality added

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -18,8 +18,8 @@ namespace ContinuousWeb.Controllers
         public IActionResult About()
 {    
 
-    //ViewData["Message"] = "Your application description page.";
-    //return View();
+    ViewData["Message"] = "Your application description page.";
+    return View();
 }
 
         public IActionResult Contact()
--- ContinuousWeb/Startup.cs
+++ ContinuousWeb/Startup.cs
@@ -6,23 +6,36 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ApplicationInsights.AspNetCore;
 
 namespace ContinuousWeb
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IHostingEnvironment env)
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+        .AddEnvironmentVariables();
+    Configuration = builder.Build();
+
+    if (env.IsDevelopment())
+    {
+        builder.AddApplicationInsightsSettings(developerMode: true);
+    }
+}
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+{
+    // Add framework services.
+    services.AddMvc();
+    services.AddApplicationInsightsTelemetry(Configuration);
+}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
